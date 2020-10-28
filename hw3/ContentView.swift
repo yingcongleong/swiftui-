6e02d4a71b953e5a623c597591a03942ebbbca13
jackson <jiechun.lai@gmail.com>
2020-10-28 10:22:50 +0800

    Initial Commit

---
+++ hw3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw3
+//
+//  Created by User02 on 2020/10/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hw3/hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  hw3App.swift
+//  hw3
+//
+//  Created by User02 on 2020/10/28.
+//
+
+import SwiftUI
+
+@main
+struct hw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
